0ee1606bba8f5374a0865343d3bb1fb00ee61aae
Burhan Keten <ketenburhan@protonmail.com>
2021-12-04 12:47:00 +0300

    day3 part2

--- examples/day3.rs
+++ examples/day3.rs
@@ -8,7 +8,7 @@ fn main() {
 
     let lines = contents.lines();
     println!("{}", part1(lines.clone()));
-    // println!("{}", part2(lines));
+    println!("{}", part2(lines));
 }
 
 fn part1(lines: Lines) -> i32 {
@@ -33,4 +33,44 @@ fn part1(lines: Lines) -> i32 {
     let gamma_rate = u16::from_str_radix(&gamma_rate, 2).unwrap();
     let epsilon_rate = u16::from_str_radix(&epsilon_rate, 2).unwrap();
     gamma_rate as i32 * epsilon_rate as i32
+}
+enum RatingType {
+    Oxy,
+    CO2,
+}
+
+fn part2(lines: Lines) -> i32 {
+    let oxy = bit_criteria(lines.clone().collect(), 0, RatingType::Oxy);
+    let co2 = bit_criteria(lines.collect(), 0, RatingType::CO2);
+    let oxy = u16::from_str_radix(oxy, 2).unwrap();
+    let co2 = u16::from_str_radix(co2, 2).unwrap();
+    oxy as i32 * co2 as i32
+}
+
+fn bit_criteria(lines: Vec<&str>, i: usize, rating_type: RatingType) -> &str {
+    let mut zeros = vec![];
+    let mut ones = vec![];
+
+    lines.iter().for_each(|line| match line.chars().nth(i) {
+        Some('0') => zeros.push(*line),
+        Some('1') => ones.push(*line),
+        _ => unreachable!(),
+    });
+    let rating_vec = if zeros.len() <= ones.len() {
+        match rating_type {
+            RatingType::Oxy => ones,
+            RatingType::CO2 => zeros,
+        }
+    } else {
+        match rating_type {
+            RatingType::Oxy => zeros,
+            RatingType::CO2 => ones,
+        }
+    };
+    let rating = if rating_vec.len() == 1 {
+        rating_vec.get(0).unwrap()
+    } else {
+        bit_criteria(rating_vec, i + 1, rating_type)
+    };
+    rating
 }
