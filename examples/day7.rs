4f086e096ddf4061a30eda2df08b8674b7b30c54
Burhan Keten <ketenburhan@protonmail.com>
2021-12-07 17:04:57 +0300

    day7

---
+++ examples/day7.rs
@@ -0,0 +1,57 @@
+use std::{collections::HashMap, fs, str::FromStr};
+
+struct Crabs(HashMap<u16, usize>);
+impl FromStr for Crabs {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut out = HashMap::new();
+        s.trim_end()
+            .split(',')
+            .map(|num_str| num_str.parse::<u16>().unwrap())
+            .for_each(|pos| {
+                let count = out.entry(pos).or_insert(0);
+                *count += 1;
+            });
+        Ok(Self(out))
+    }
+}
+
+fn main() {
+    let contents =
+        fs::read_to_string("./inputs/day7.txt").expect("Something went wrong reading the file");
+    let crabs = Crabs::from_str(&contents).unwrap();
+
+    println!("{:#?}", crabs.0);
+
+    println!("part1: {}", answer(1, &crabs));
+    println!("part2: {}", answer(2, &crabs));
+}
+
+fn answer(part: u8, crabs: &Crabs) -> usize {
+    (0..=2000)
+        .into_iter()
+        .map(|pos| {
+            crabs
+                .0
+                .iter()
+                .map(|(&old_pos, &count)| {
+                    let diff = if old_pos > pos {
+                        old_pos - pos
+                    } else {
+                        pos - old_pos
+                    };
+                    match part {
+                        1 => diff as usize * count,
+                        2 => match diff {
+                            n if n > 1 => (1..=n as usize).sum::<usize>() * count,
+                            1 => count,
+                            _ => 0,
+                        },
+                        _ => unreachable!(),
+                    }
+                })
+                .sum()
+        })
+        .min()
+        .unwrap()
+}
